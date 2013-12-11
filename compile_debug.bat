28abc6c27a1bbec4303487e4744f0c8d918da1cc
Naoya Komatsu <komatsu@biscrat.com>
2013-12-12 03:32:22 +0900

    Add function to compile in release mode.

--- compile_debug.bat
+++ compile_debug.bat
@@ -1,1 +1,1 @@
-tvpwin32.exe "tools" -startup="compile.tjs" -readencoding="Shift_JIS" -compiledebug="yes"
+tvpwin32.exe "tools" -startup="compile.tjs" -readencoding="Shift_JIS" -compiledebug="yes" -compilerelease="no" -clean="no"
---
+++ compile_release.bat
@@ -0,0 +1,1 @@
+tvpwin32.exe "tools" -startup="compile.tjs" -readencoding="Shift_JIS" -compiledebug="no" -compilerelease="yes" -clean="no
